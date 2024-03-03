1b8a0a859f788e11e64adc77f33359de4e8b0c48
jcodes89 <jeremy.sluder89@gmail.com>
2024-03-03 11:30:33 -0500

    About section update

--- app/components/About/AboutCard.js
+++ app/components/About/AboutCard.js
@@ -4,7 +4,7 @@ import React from 'react'
 const AboutCard = (props) => {
   return (
     <>
-      <div className='bg-gray-300 text-gray-950 p-4 m-3 rounded-badge shadow-secondary shadow-sm'>
+      <div className=' text-gray-950 p-3 my-5 lg:m-2 rounded-md shadow-secondary shadow-sm'>
         <h3 className='underline text-lg lg:text-xl pb-1'>{props.heading}</h3>
         <p className='pb-3 lg:pb-5 '>{props.paragraph}</p>
       </div>
