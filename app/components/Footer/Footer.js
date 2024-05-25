943c89d0a9c6ea76b7ae7494fbb72af33e01c7d4
jcodes89 <jeremy.sluder89@gmail.com>
2024-05-25 11:05:40 -0400

    update footer links

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -11,8 +11,8 @@ const Footer = () => {
           <p className='text-sm'>Tampa, FL</p>
         </div>
         <div className='md:hidden flex flex-row justify-center w-full left-3 py-1'>
-          <Link className='' href='https://www.instagram.com/davinchleatherco/'><i className=" mx-3 fa-brands fa-square-instagram text-secondary scale-150 hover:text-accent hover:scale-[2]"></i></Link>
-          <Link className='' href='https://www.facebook.com/davinchleatherco/'><i className=" mx-3 fa-brands fa-square-facebook text-secondary scale-150 hover:text-accent hover:scale-[2]"></i></Link>
+          <Link className='' aria-label='Instagram profile link' href='https://www.instagram.com/davinchleatherco/'><i className="aria-label='Instagram profile link' mx-3 fa-brands fa-square-instagram text-secondary scale-150 hover:text-accent hover:scale-[2]"></i></Link>
+          <Link className='' aria-label='Facebook profile link' href='https://www.facebook.com/davinchleatherco/'><i className="aria-label='Facebook profile link' mx-3 fa-brands fa-square-facebook text-secondary scale-150 hover:text-accent hover:scale-[2]"></i></Link>
         </div>
       </footer>
     </>
