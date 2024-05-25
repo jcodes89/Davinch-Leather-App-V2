ce8ee41bfd0b58f4806abdb65ba5c07b3d6b7c17
jcodes89 <jeremy.sluder89@gmail.com>
2024-05-25 11:17:42 -0400

    update to loader page

---
+++ app/loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading(){
+    return(
+        <div className=" h-screen w-screen flex justify-center items-center bg-white flex-col">
+            <span className="loading loading-infinity loading-lg scale-150 relative z-50 text-gray-50 "></span>
+        </div>
+    )
+}
